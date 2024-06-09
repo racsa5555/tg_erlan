f0c8b9b28c5b1b27ccb69ae54aa98af04db47347
ascar <ascar@gmail.com>
2024-06-09 18:32:32 +0300

    fixed error

--- dishes.py
+++ dishes.py
@@ -1,12 +1,12 @@
 from PIL import Image
 
-d1_ru = {'id':1,'title':'Каша','reciept':'Это рецепт по приготовлению Каши','img':'./images/d1.jpeg'}
-d2_ru = {'id':2,'title':'Суп','reciept':'Это рецепт по приготовлению Борща ','img':'./images/d2.jpeg'}
-d3_ru = {'id':3,'title':'Блины','reciept':'Это рецепт по приготовлению Блинов ','img':'./images/d3.jpeg'}
+d1_ru = {'id':1,'title':'Каша','reciept':'Это рецепт по приготовлению Каши','img':'./images/d1.jpg'}
+d2_ru = {'id':2,'title':'Суп','reciept':'Это рецепт по приготовлению Борща ','img':'./images/d2.jpg'}
+d3_ru = {'id':3,'title':'Блины','reciept':'Это рецепт по приготовлению Блинов ','img':'./images/d3.jpg'}
 
-d1_en = {'id':1,'title':'Porridge','reciept':'This is reciept for porridge','img':'./images/d1.jpeg'}
-d2_en = {'id':2,'title':'Soup','reciept':'This is reciept for soup','img':'./images/d2.jpeg'}
-d3_en = {'id':3,'title':'Pancakes','reciept':'This is reciept for pancakes','img':'./images/d3.jpeg'}
+d1_en = {'id':1,'title':'Porridge','reciept':'This is reciept for porridge','img':'./images/d1.jpg'}
+d2_en = {'id':2,'title':'Soup','reciept':'This is reciept for soup','img':'./images/d2.jpg'}
+d3_en = {'id':3,'title':'Pancakes','reciept':'This is reciept for pancakes','img':'./images/d3.jpg'}
 
 static_t = {'ru':'Описание','en':'Title'}
 static_r = {'ru':'Рецепт','en':'Reciept'}
